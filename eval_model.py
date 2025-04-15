24cb2aad71d8b51d8313d0e59319f4a3af3b1d3e
xy3 <120182408@qq.com>
2025-04-15 11:32:28 +0800

    加载权重位置提示日志

--- eval_model.py
+++ eval_model.py
@@ -18,7 +18,7 @@ def init_model(args):
         moe_path = '_moe' if args.use_moe else ''
         modes = {0: 'pretrain', 1: 'full_sft', 2: 'rlhf', 3: 'reason', 4: 'grpo'}
         ckp = f'./{args.out_dir}/{modes[args.model_mode]}_{args.dim}{moe_path}.pth'
-
+        print(f"加载权重{ckp}")
         model = MiniMindLM(LMConfig(
             dim=args.dim,
             n_layers=args.n_layers,
